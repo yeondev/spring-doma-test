cceeb40130f4bab99da3b2c58a7e29514726c35e
韓 道煙 <han@vnlhome.local>
2018-10-16 15:18:44 +0900

    test

--- src/main/java/com/example/sample/domain/repository/SampleRepository.java
+++ src/main/java/com/example/sample/domain/repository/SampleRepository.java
@@ -5,11 +5,13 @@ import com.example.sample.domain.model.SampleEntity;
 import org.seasar.doma.Dao;
 import org.seasar.doma.Select;
 import org.seasar.doma.boot.ConfigAutowireable;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @ConfigAutowireable
 @Dao
+@Repository
 public interface SampleRepository {
 
     @Select
